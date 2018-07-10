cd136b0d4359a97abd99dc58d773c7dd7b4ff8a3
marcelooliveira <mclricardo@gmail.com>
2018-07-10 00:15:15 -0300

    EmailService logging

--- CourseSignUp/CourseSignUp.Domain/Services/EmailService.cs
+++ CourseSignUp/CourseSignUp.Domain/Services/EmailService.cs
@@ -1,4 +1,5 @@
 ﻿using CourseSignUp.Domain.Model;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,19 +8,25 @@ namespace CourseSignUp.Domain.Services
 {
     public class EmailService : IEmailService
     {
+        private readonly ILogger logger;
+        public EmailService(ILogger<EmailService> logger)
+        {
+            this.logger = logger;
+        }
+
         public void SendSignupSuccess(Course course, SignUpInput signUpInput)
         {
-            Console.WriteLine($"SUCCESS sign up email was sent to: {signUpInput.Name}");
+            logger.LogInformation($"SUCCESS sign up email was sent to: {signUpInput.Name}");
         }
 
         public void SendSignupEnrollmentFull(Course course, SignUpInput signUpInput)
         {
-            Console.WriteLine($"ENROLLMENT_FULL email was sent to: {signUpInput.Name}");
+            logger.LogInformation($"ENROLLMENT_FULL email was sent to: {signUpInput.Name}");
         }
 
         public void SendSignupStudentAlreadyEnrolled(Course course, SignUpInput signUpInput)
         {
-            Console.WriteLine($"STUDENT_ALREADY_ENROLLED email was sent to: {signUpInput.Name}");
+            logger.LogInformation($"STUDENT_ALREADY_ENROLLED email was sent to: {signUpInput.Name}");
         }
     }
 }
