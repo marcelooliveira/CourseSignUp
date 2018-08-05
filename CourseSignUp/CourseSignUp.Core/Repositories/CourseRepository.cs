1271827a0debab1e030de730cf22afaf06b28c1a
marcelooliveira <mclricardo@gmail.com>
2018-08-05 02:26:26 -0300

    catch DbUpdateConcurrencyException

--- CourseSignUp/CourseSignUp.Core/Repositories/CourseRepository.cs
+++ CourseSignUp/CourseSignUp.Core/Repositories/CourseRepository.cs
@@ -51,7 +51,43 @@ namespace CourseSignUp.Data.Repositories
 
             context.Set<Enrollment>().Add(new Enrollment(course, student));
 
-            await context.SaveChangesAsync();
+            var saved = false;
+
+            while (!saved)
+            {
+                try
+                {
+                    await context.SaveChangesAsync();
+                    saved = true;
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    foreach (var entry in ex.Entries)
+                    {
+                        if (entry.Entity is Enrollment)
+                        {
+                            var proposedValues = entry.CurrentValues;
+                            var databaseValues = entry.GetDatabaseValues();
+
+                            foreach (var property in proposedValues.Properties)
+                            {
+                                var proposedValue = proposedValues[property];
+                                var databaseValue = databaseValues[property];
+
+                                // TODO: decide which value should be written to database
+                                // proposedValues[property] = <value to be saved>;
+                            }
+                            entry.OriginalValues.SetValues(databaseValues);
+                        }
+                        else
+                        {
+                            throw new NotSupportedException(
+                                $"Don't know how to handle exception for '{entry.Metadata.Name}'");
+                        }
+                    }
+                }
+            }
+
         }
 
         public async Task UpdateCourseStats(string courseCode, int studentCount, DateTime? minBirthdate, DateTime? maxBirthdate, long birthdateTickSum)
